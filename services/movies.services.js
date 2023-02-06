8b0f45f34dd43af0a810b4e361e8abfc26c36d1e
Saravanan-1213mm <saravanansekar033@gmail.com>
2023-02-06 14:38:13 +0530

    id to ObjectId

--- services/movies.services.js
+++ services/movies.services.js
@@ -1,16 +1,24 @@
 import { client } from "../index.js";
+import { ObjectId } from "mongodb";
 
 export async function getMoviesById(id) {
-  return await client.db("b40wd").collection("movies").findOne({ id: id });
+  console.log("****", id);
+  return await client
+    .db("b40wd")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 export async function updateMovieById(id, data) {
   return await client
     .db("b40wd")
     .collection("movies")
-    .updateOne({ id: id }, { $set: data });
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
-  return await client.db("b40wd").collection("movies").deleteOne({ id: id });
+  return await client
+    .db("b40wd")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
 export async function getMovies() {
   return await client.db("b40wd").collection("movies").find({}).toArray();
